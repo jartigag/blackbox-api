f2721686845b82b1ac9551144ab7574f494fc49c
jartigag <jartigag@protonmail.com>
2019-01-10 11:13:19 +0100

    done: post on mastodon

---
+++ blackbox-api.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# let's detach bots from platforms
+#
+# usage: python3 blackbox-api.py post whatToPost --mastodon
+
+__author__ = "@jartigag"
+__version__ = "0.1"
+
+import argparse
+from mastodon import Mastodon
+import secrets
+
+masto_account = Mastodon( access_token=secrets.masto_access_token, api_base_url='https://botsin.space')
+
+def post(content,mastodon,verbose):
+	"""explain this"""
+	post=''
+	if mastodon:
+		try:
+			post = masto_account.toot(content)
+			if verbose: print('%s - \033[1mtooted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
+				(post.created_at.strftime('%Y-%m-%d %H:%M:%S'),post.content,post.url))
+		except Exception as e:
+			if verbose: print("\n[\033[91m!\033[0m] error: %s" % e)
+	return post
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(
+		description="let's detach from platforms, v%s by @%s" % (__version__,__author__),
+		usage="%(prog)s post whatToPost --mastodon")
+	parser.add_argument('action',choices=['post'],
+		help='what to do')
+	parser.add_argument('content',
+		help="action's text/content")
+	parser.add_argument('-m','--mastodon',action='store_true',
+		help='where: mastodon')
+	parser.add_argument('-v','--verbose',action='store_true',
+		help='to print or not to print')
+	args = parser.parse_args()
+
+	if args.action=='post':
+		post(args.content,args.mastodon,args.verbose)
