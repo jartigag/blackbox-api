8e93a8e30824108d591e205b4fcf6a0f49d2bd55
jartigag <jartigag@pm.me>
2019-01-15 00:33:59 +0100

    done: post (either chat or channel) on telegram

--- blackbox-api.py
+++ blackbox-api.py
@@ -11,35 +11,41 @@ import argparse
 from mastodon import Mastodon
 from telegram import Bot
 import secrets
+from datetime import datetime
 
 masto_account = Mastodon( access_token=secrets.masto_access_token, api_base_url='https://botsin.space')
 tele_bot = Bot(token=secrets.telegram_token)
-tele_bot.chat_id=secrets.telegram_chat_id
+tele_bot.chat_id = secrets.telegram_chat_id
 
-def post(content,mastodon,telegram,verbose):
+def post(content,mastodon=False,telegram=False,verbose=False):
 	"""post some content on any platform
 
 		args:
 			content (str): what to post
 			mastodon (bool): if True, post it on Mastodon
+			telegram (bool): if True, post it (either on a chat or a channel) on Telegram
 			verbose (bool): if True, print post info
 
 		returns:
 			the post object
 		"""
-	post=''
+	post = True
 	if mastodon:
 		try:
 			post = masto_account.toot(content)
 			if verbose: print('%s - \033[1mtooted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
 				(post.created_at.strftime('%Y-%m-%d %H:%M:%S'),post.content,post.url))
 		except Exception as e:
-			if verbose: print("\n[\033[91m!\033[0m] error: %s" % e)
+			post = False
+			if verbose: print("\n[\033[91m!\033[0m] mastodon error: %s" % e)
 	if telegram:
-			try:
-				tele_bot.send_message(chat_id=tele_bot.chat_id, text=content)
-			except Exception as e: 
-				if verbose: print("\n[\033[91m!\033[0m] error: %s" % e)
+		try:
+			tele_bot.send_message(chat_id=tele_bot.chat_id, text=content)
+			if verbose: print('%s - \033[1mtooted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
+				(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),content,tele_bot.chat_id))
+		except Exception as e:
+			post = False
+			if verbose: print("\n[\033[91m!\033[0m] telegram error: %s" % e)
 	return post
 
 if __name__ == '__main__':
@@ -60,4 +66,4 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	if args.action=='post':
-		post(args.content,args.mastodon,args.verbose)
+		post(args.content,args.mastodon,args.telegram,args.verbose)
