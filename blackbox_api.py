f5b069c3c53a01671545b3ffadacea473d3d2859
jartigag <jartigag@pm.me>
2019-02-07 22:22:21 +0100

    wip: no need to use wrappers. python-telegram-bot removed

--- blackbox_api.py
+++ blackbox_api.py
@@ -9,87 +9,84 @@ __version__ = "0.1"
 
 import argparse
 from mastodon import Mastodon
-from telegram import Bot
 from tweepy import OAuthHandler, API
 import secrets
 from datetime import datetime
+import urllib.request
 
 try:
-	masto_account = Mastodon(access_token=secrets.masto_access_token, api_base_url='https://botsin.space')
+    masto_account = Mastodon(access_token=secrets.masto_access_token, api_base_url='https://botsin.space')
 except Exception as e:
-	pass
+    pass
 try:
-	tele_bot = Bot(token=secrets.telegram_token)
-	tele_bot.chat_id = secrets.telegram_chat_id
+    auth = OAuthHandler(secrets.twitter_consumer_key, secrets.twitter_consumer_secret)
+    auth.set_access_token(secrets.twitter_access_token, secrets.twitter_access_token_secret)
+    twitter_api = API(auth)
 except Exception as e:
-	pass
-try:
-	auth = OAuthHandler(secrets.twitter_consumer_key, secrets.twitter_consumer_secret)
-	auth.set_access_token(secrets.twitter_access_token, secrets.twitter_access_token_secret)
-	twitter_api = API(auth)
-except Exception as e:
-	pass
+    pass
 
 def post(content,twitter=False,mastodon=False,telegram=False,verbose=False):
-	"""post some content on any platform
+    """post some content on any platform
 
-		args:
-			content (str): what to post
-						twitter (bool): if True, post it on Twitter
-			mastodon (bool): if True, post it on Mastodon
-			telegram (bool): if True, post it (either on a chat or a channel) on Telegram
-			verbose (bool): if True, print post info
+        args:
+            content (str): what to post
+                        twitter (bool): if True, post it on Twitter
+            mastodon (bool): if True, post it on Mastodon
+            telegram (bool): if True, post it (either on a chat or a channel) on Telegram
+            verbose (bool): if True, print post info
 
-		returns:
-			the post object (an status if twitter=True, a toot if mastodon=True; if error, post=False)
-		"""
-	post = True
-	if twitter:
-		try:
-			post = twitter_api.update_status(content)
-			tweet_url = '{}/{}/status/{}'.format(post.source_url,post.user.screen_name,post.id)
-			if verbose: print('%s - \033[1mtweeted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
-				(post.created_at.strftime('%Y-%m-%d %H:%M:%S'),post.text,tweet_url))
-		except Exception as e:
-			post = False
-			if verbose: print("\n[\033[91m!\033[0m] twitter error: %s" % e)
-	if mastodon:
-		try:
-			post = masto_account.toot(content)
-			url = '{}/{}/{}'.format(post.source_url,post.user.screen_name,post.id)
-			if verbose: print('%s - \033[1mtooted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
-				(post.created_at.strftime('%Y-%m-%d %H:%M:%S'),post.content,url))
-		except Exception as e:
-			post = False
-			if verbose: print("\n[\033[91m!\033[0m] mastodon error: %s" % e)
-	if telegram:
-		try:
-			tele_bot.send_message(chat_id=tele_bot.chat_id, text=content)
-			if verbose: print('%s - \033[1mtooted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
-				(datetime.now().strftime('%Y-%m-%d %H:%M:%S'),content,tele_bot.chat_id))
-		except Exception as e:
-			post = False
-			if verbose: print("\n[\033[91m!\033[0m] telegram error: %s" % e)
-	return post
+        returns:
+            the post object (an status if twitter=True, a toot if mastodon=True; if error, post=False)
+        """
+    global secrets
+    post = True
+    if twitter:
+        try:
+            post = twitter_api.update_status(content)
+            tweet_url = '{}/{}/status/{}'.format(post.source_url,post.user.screen_name,post.id)
+            if verbose: print('%s - \033[1mtweeted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
+                (post.created_at.strftime('%Y-%m-%d %H:%M:%S'),post.text,tweet_url))
+        except Exception as e:
+            post = False
+            if verbose: print("\n[\033[91m!\033[0m] twitter error: %s" % e)
+    if mastodon:
+        try:
+            post = masto_account.toot(content)
+            url = '{}/{}/{}'.format(post.source_url,post.user.screen_name,post.id)
+            if verbose: print('%s - \033[1mtooted "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
+                (post.created_at.strftime('%Y-%m-%d %H:%M:%S'),post.content,url))
+        except Exception as e:
+            post = False
+            if verbose: print("\n[\033[91m!\033[0m] mastodon error: %s" % e)
+    if telegram:
+        try:
+            urllib.request.urlopen( "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(\
+                    secrets.telegram_token, secrets.telegram_chat_id, content) )
+            if verbose: print('%s - \033[1msent by telegram "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
+                (datetime.now().strftime('%Y-%m-%d %H:%M:%S'),content,secrets.telegram_chat_id))
+        except Exception as e:
+            post = False
+            if verbose: print("\n[\033[91m!\033[0m] telegram error: %s" % e)
+    return post
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(
-		description="let's detach from platforms, v%s by @%s" % (__version__,__author__),
-		usage="%(prog)s post 'content of the post' [--mastodon --telegram --twitter] [-v]")
-	parser.add_argument('action',choices=['post'],
-		help='what to do')
-	parser.add_argument('content',
-		help="action's text/content")
-	parser.add_argument('-m','--mastodon',action='store_true',
-		help='where: mastodon')
-	parser.add_argument('-tg','--telegram',action='store_true',
-		help='where: telegram')
-	parser.add_argument('-tw','--twitter',action='store_true',
-		help='where: twitter')
-	parser.add_argument('-v','--verbose',action='store_true',
-		help='to print or not to print')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="let's detach from platforms, v%s by %s" % (__version__,__author__),
+        usage="%(prog)s post 'content of the post' [--mastodon --telegram --twitter] [-v]")
+    parser.add_argument('action',choices=['post'],
+        help='what to do')
+    parser.add_argument('content',
+        help="action's text/content")
+    parser.add_argument('-m','--mastodon',action='store_true',
+        help='where: mastodon')
+    parser.add_argument('-tg','--telegram',action='store_true',
+        help='where: telegram')
+    parser.add_argument('-tw','--twitter',action='store_true',
+        help='where: twitter')
+    parser.add_argument('-v','--verbose',action='store_true',
+        help='to print or not to print')
+    args = parser.parse_args()
 
-	if args.action=='post':
-		post(args.content,args.twitter,args.mastodon,args.telegram,args.verbose)
+    if args.action=='post':
+        post(args.content,args.twitter,args.mastodon,args.telegram,args.verbose)
