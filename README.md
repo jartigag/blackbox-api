32add414cd9475786b621cf634132689abae205e
jartigag <jartigag@pm.me>
2019-02-11 23:14:51 +0100

    added: reply_ops

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 
 just a library i'm working on to easily interact (post, follow, etc.) with [twitter](https://developer.twitter.com/en/docs/api-reference-index), [mastodon](https://docs.joinmastodon.org/), [telegram](https://core.telegram.org/bots/api) or any other platform or interest.
 
+to use this library anywhere, execute:
+```
+export PYTHONPATH=$(pwd)
+```
+
 ### dependencies
 i'm using some wrappers in order to develop this faster:
 - [tweepy](https://github.com/tweepy/tweepy)
--- blackbox_api.py
+++ blackbox_api.py
@@ -25,12 +25,12 @@ try:
 except Exception as e:
     pass
 
-def post(content,twitter=False,mastodon=False,telegram=False,verbose=False):
+def post(content,twitter=False,mastodon=False,telegram=False,reply_ops=[],verbose=False):
     """post some content on any platform
 
         args:
             content (str): what to post
-                        twitter (bool): if True, post it on Twitter
+            twitter (bool): if True, post it on Twitter
             mastodon (bool): if True, post it on Mastodon
             telegram (bool): if True, post it (either on a chat or a channel) on Telegram
             verbose (bool): if True, print post info
@@ -59,9 +59,11 @@ def post(content,twitter=False,mastodon=False,telegram=False,verbose=False):
             post = False
             if verbose: print("\n[\033[91m!\033[0m] mastodon error: %s" % e)
     if telegram:
+        if reply_ops:
+            reply_markup = '&reply_markup={"keyboard":[{}]}'.format( '[%s]' % ', '.join(map(str, reply_ops)) )
         try:
-            urllib.request.urlopen( "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}".format(\
-                    secrets.telegram_token, secrets.telegram_chat_id, content) )
+            urllib.request.urlopen( "https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}{}".format(\
+                    secrets.telegram_token, secrets.telegram_chat_id, content, reply_markup) )
             if verbose: print('%s - \033[1msent by telegram "\033[0m%s\033[1m" (in \033[0m%s\033[1m)\033[0m' %
                 (datetime.now().strftime('%Y-%m-%d %H:%M:%S'),content,secrets.telegram_chat_id))
         except Exception as e:
